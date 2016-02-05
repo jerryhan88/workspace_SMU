9a22472c23ee8a1980bd4838f98cfc5d87d3c8e9
jerryhan88 <jerryhan88@gmail.com>
2016-02-05 19:47:05 +0800

    t

--- taxi_sh/check_os_and_set_prefix.sh
+++ taxi_sh/check_os_and_set_prefix.sh
@@ -2,7 +2,7 @@
 
 os_type=`uname -a | awk '{print $1}'`
  
-if [ $os_type=Darwin ]; then
+if [ $os_type == Darwin ]; then
 	echo $os_type /Users/JerryHan88/taxi
 else
 	echo $os_type /home/ckhan/taxi 
