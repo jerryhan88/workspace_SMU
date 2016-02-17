8f837b81d81d3a31e3fa916df9712c75053782ba
jerryhan88 <jerryhan88@gmail.com>
2016-02-17 16:42:33 +0800

    t

--- Easy_21/_setting.py
+++ Easy_21/_setting.py
@@ -1,5 +1,6 @@
 from __future__ import division
 from math import sqrt
+from random import random, choice
 BLACK, RED = range(2)
 BLACK_PROB, RED_PROB = 2 / 3, 1 / 3
 CARDS_NUM = range(1, 11)
@@ -17,3 +18,14 @@ GR_dev_2 = [pow(v - sum(GAME_RESULT)/len(GAME_RESULT), 2) for v in GAME_RESULT]
 CONVERGENCE_CONDITION = sqrt(sum(GR_dev_2)/len(GR_dev_2)) 
 #
 Q_LEARNING_DIR = 'Q_learning_results'
+SUMMARY_FNAME = 'result_q_learning.csv'
+#
+get_sum = lambda cards: sum([cn for cc, cn in cards if cc == BLACK]) - sum([cn for cc, cn in cards if cc == RED])
+
+def get_card():
+    # cc: card's color
+    # cn: card's number
+    cc = RED if random() <= RED_PROB else BLACK 
+    cn = choice(CARDS_NUM)
+    return cc, cn
+
