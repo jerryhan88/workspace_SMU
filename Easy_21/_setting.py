d67bb373474c5eb3762b412bd9ae77b38de08d58
jerryhan88 <jerryhan88@gmail.com>
2016-02-17 11:56:29 +0800

    t

--- Easy_21/_setting.py
+++ Easy_21/_setting.py
@@ -6,11 +6,14 @@ CARDS_NUM = range(1, 11)
 #
 STICK, HIT = range(2)
 GAME_RESULT = WIN, DRAW, LOSE = 1, 0, -1
+BUST = 'BUST'
 #
 PLAYER_STATES = range(1, 21)
 DEALER_STATES = range(1, 11)
 #
-ALPHA, GAMMA = 0.08, 0.5
+ALPHA, GAMMA = 0.1, 0.5
 #
 GR_dev_2 = [pow(v - sum(GAME_RESULT)/len(GAME_RESULT), 2) for v in GAME_RESULT]
 CONVERGENCE_CONDITION = sqrt(sum(GR_dev_2)/len(GR_dev_2)) 
+#
+Q_LEARNING_DIR = 'Q_learning_results'
