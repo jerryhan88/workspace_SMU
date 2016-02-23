67c63fe0e9726637cdc92fabda5ed679d98f4fce
jerryhan88 <jerryhan88@gmail.com>
2016-02-23 17:12:28 +0800

    t

--- taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
+++ taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
@@ -1,7 +1,11 @@
-prefix = '/home/sfcheng/toolbox'
-
+from __future__ import division
+#
 import os
+#
+prefix = '/home/sfcheng/toolbox'
 
 cvs_files = [fn for fn in os.listdir(prefix) if fn.startswith('shift') and fn.endswith('.csv')]
 for x in cvs_files:
-    print x
+    _,_,_, yymm = x[:-len('.csv')].split('-')
+    
+    print yymm, x 
