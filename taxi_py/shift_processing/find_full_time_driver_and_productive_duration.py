75890d78a93dca49e0c62f36b3df03e6b5f74955
jerryhan88 <jerryhan88@gmail.com>
2016-02-24 10:26:24 +0800

    t

--- taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
+++ taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
@@ -4,7 +4,7 @@ sys.path.append(os.getcwd() + '/..')
 #
 from support._setting import ms_dir
 from support.logger import logging_msg
-# from support.multiprocess import init_multiprocessor, put_task, end_multiprocessor
+from support.multiprocess import init_multiprocessor, put_task, end_multiprocessor
 from traceback import format_exc
 #
 prefix = '/home/sfcheng/toolbox'
@@ -14,20 +14,20 @@ def run():
     if os.path.exists(ms_dir):
         shutil.rmtree(ms_dir)
     os.makedirs(ms_dir)
-#     init_multiprocessor()
-#     count_num_jobs = 0
+    init_multiprocessor()
+    count_num_jobs = 0
     cvs_files = [fn for fn in os.listdir(prefix) if fn.startswith('shift') and fn.endswith('.csv')]
     for fn in cvs_files:
         _, _, _, yymm = fn[:-len('.csv')].split('-')
         if yymm.startswith('11') or yymm.startswith('12'):
             continue
         try:
-            process_file(fn, yymm)
+            put_task(process_file, [fn, yymm])
         except Exception as _:
-            logging_msg('Algorithm runtime exception (%s%s)\n' % (yymm) + format_exc())
+            logging_msg('Algorithm runtime exception (%s)\n' % (yymm) + format_exc())
             raise
-#         count_num_jobs += 1
-#     end_multiprocessor(count_num_jobs)
+        count_num_jobs += 1
+    end_multiprocessor(count_num_jobs)
 
 
 def process_file(fn, yymm):        
@@ -47,7 +47,7 @@ def process_file(fn, yymm):
         productive_state = [id_dur0, id_dur3, id_dur4, id_dur5, id_dur6, id_dur7, id_dur8, id_dur9, id_dur10]
         with open('%s/temp_%s' % (ms_dir, fn), 'wt') as w_csvfile:
             writer = csv.writer(w_csvfile)
-            new_headers = ['year', 'month', 'day', 'hour', 'driver-id', 'productive-duration', 'x-productive-duration']
+            new_headers = ['year', 'month', 'day', 'hour', 'vehicle-id', 'driver-id', 'productive-duration', 'x-productive-duration']
             writer.writerow(new_headers)
             for row in reader:
                 vid, did = row[id_vid], row[id_did] 
@@ -56,22 +56,25 @@ def process_file(fn, yymm):
                 is_driver_vehicle[vid].append(did)
                 productive_duration = sum(int(row[x]) for x in productive_state)
                 x_productive_duration = sum(int(row[x]) for x in x_productive_state)
-                writer.writerow([row[id_year], row[id_month], row[id_day], row[id_hour], did, productive_duration, x_productive_duration])
+                writer.writerow([row[id_year], row[id_month], row[id_day], row[id_hour], vid, did, productive_duration, x_productive_duration])
     #
     with open('%s/temp_%s' % (ms_dir, fn), 'rt') as r_csvfile:
         reader = csv.reader(r_csvfile)
         headers = reader.next()
-        id_vid = headers.index('vehicle-id')
+        id_year, id_month, id_day, id_hour = headers.index('year'), headers.index('month'), headers.index('day'), headers.index('hour')
+        id_vid, id_did = headers.index('vehicle-id'), headers.index('driver-id')
         id_productive, id_x_productive = headers.index('productive-duration'), headers.index('x-productive-duration')
         with open('%s/%s' % (ms_dir, fn), 'wt') as w_csvfile:
             writer = csv.writer(w_csvfile)
-            writer.writerow(headers)
+            new_headers = ['year', 'month', 'day', 'hour', 'driver-id', 'productive-duration', 'x-productive-duration']
+            writer.writerow(new_headers)
             for row in reader:
                 if len(is_driver_vehicle[row[id_vid]]) > 1:
                     continue
-                if int(row[id_productive]) == 0 and int(row[id_x_productive]) == 0:
+                productive_duration, x_productive_duration = int(row[id_productive]), int(row[id_x_productive]) 
+                if productive_duration == 0 and x_productive_duration == 0:
                     continue
-                writer.writerow(row)
+                writer.writerow([row[id_year], row[id_month], row[id_day], row[id_hour], did, productive_duration, x_productive_duration])
     os.remove('%s/temp_%s' % (ms_dir, fn))
     print 'end the file; %s' % yymm
     logging_msg('end the file; %s' % yymm)
