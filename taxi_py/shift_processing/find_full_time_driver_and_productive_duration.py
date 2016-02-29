14d790e1cdf101eb838475c48d61aa41a01dada2
jerryhan88 <jerryhan88@gmail.com>
2016-02-29 10:10:33 +0800

    t

--- taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
+++ taxi_py/shift_processing/find_full_time_driver_and_productive_duration.py
@@ -68,7 +68,7 @@ def process_file(fn, yymm):
         id_productive, id_x_productive = headers.index('productive-duration'), headers.index('x-productive-duration')
         with open('%s/%s' % (ms_dir, fn), 'wt') as w_csvfile:
             writer = csv.writer(w_csvfile)
-            new_headers = ['year', 'month', 'day', 'hour', 'driver-id', 'productive-duration', 'x-productive-duration']
+            new_headers = ['year', 'month', 'day', 'hour', 'vehicle-id', 'driver-id', 'productive-duration', 'x-productive-duration']
             writer.writerow(new_headers)
             for row in reader:
                 if len(is_driver_vehicle[row[id_vid]]) > 1:
@@ -76,7 +76,7 @@ def process_file(fn, yymm):
                 productive_duration, x_productive_duration = int(row[id_productive]), int(row[id_x_productive]) 
                 if productive_duration == 0 and x_productive_duration == 0:
                     continue
-                writer.writerow([row[id_year], row[id_month], row[id_day], row[id_hour], row[id_did], productive_duration, x_productive_duration])
+                writer.writerow([row[id_year], row[id_month], row[id_day], row[id_hour], row[id_vid], row[id_did], productive_duration, x_productive_duration])
 #     os.remove('%s/temp_%s' % (ms_dir, fn))
     print 'end the file; %s' % yymm
     logging_msg('end the file; %s' % yymm)
