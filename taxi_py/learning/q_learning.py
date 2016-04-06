b2c8d4e6cbf3b2560e846b830cc5c887c9fa2bf3
jerryhan88 <jerryhan88@gmail.com>
2016-04-06 15:57:52 +0800

    t

--- taxi_py/learning/q_learning.py
+++ taxi_py/learning/q_learning.py
@@ -17,18 +17,26 @@ import csv, datetime
 MOD_STAN = 100000
 #
 def run():
+    
+    task_list = [(0, 0), (0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (0, 7), (0, 8), (0, 9), (0, 10), (2, 0), (2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (2, 10), (4, 0), (4, 1), (4, 2), (4, 3), (4, 4), (4, 5), (4, 6), (4, 7), (4, 8), (4, 9), (4, 10), (6, 0), (6, 1), (6, 2), (6, 3), (6, 4), (6, 5), (6, 6), (6, 7), (6, 8), (6, 9), (6, 10), (8, 0), (8, 1), (8, 2), (8, 3), (8, 4), (8, 5), (8, 6), (8, 7), (8, 8), (8, 9), (8, 10), (10, 0), (10, 1), (10, 2), (10, 3), (10, 4), (10, 5), (10, 6), (10, 7), (10, 8), (10, 9), (10, 10)]
     init_multiprocessor()
     counter = 0
-    for i in range(1,11,2):
-#     for i in range(9,0,-2): 
-        ALPHA = i / 10 
-        for j in range(1,11,2):
-#         for j in range(9,0,-2):
-            GAMMA = j / 10
-#             process_files(ALPHA, GAMMA)
-            put_task(process_files, [ALPHA, GAMMA])
-            counter += 1
-    end_multiprocessor(counter)
+    for i, j in task_list:
+        ALPHA = i / 10
+        GAMMA = j / 10
+        put_task(process_files, [ALPHA, GAMMA])
+        counter += 1
+    end_multiprocessor(counter)     
+#     for i in range(1,11,2):
+# #     for i in range(9,0,-2): 
+#         ALPHA = i / 10 
+#         for j in range(1,11,2):
+# #         for j in range(9,0,-2):
+#             GAMMA = j / 10
+# #             process_files(ALPHA, GAMMA)
+#             put_task(process_files, [ALPHA, GAMMA])
+#             counter += 1
+#     end_multiprocessor(counter)
     
 def process_files(ALPHA, GAMMA):
     ALPHA_GAMMA_dir = for_learning_dir + '/ALPHA-%.2f-GAMMA-%.2f' % (ALPHA, GAMMA)
